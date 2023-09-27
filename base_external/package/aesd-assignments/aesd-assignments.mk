ea88b3a555a8fe107bf29a7a574e5f721c92c2a1
Doug Weber <dweber641@gmail.com>
2023-09-26 21:32:28 -0400

    Completed assignment 8

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 # Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 122016c44aa630ef71b71d8995e568d7d61508ee
+AESD_ASSIGNMENTS_VERSION = 561eb973227f0fb8e1b0f4282ee2805bd620deb9
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -31,4 +31,7 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
+AESD_ASSIGNMENTS_MODULE_SUBDIRS = aesd-char-driver
+
+$(eval $(kernel-module))
 $(eval $(generic-package))
